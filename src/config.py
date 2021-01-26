7bf2f3a20ef06f637bda5a390dd92ca60c488ede
Marvin-chengmin <marvin.cmin@gmail.com>
2021-01-26 12:27:32 -0700

    Finished the script to train the model and log the result

--- src/config.py
+++ src/config.py
@@ -1,2 +1,20 @@
+# Data processing config
 LABELS = ['opacity', 'normal']
 IMG_SIZE = 128
+
+# Model training hyperparematers
+HIDDEN_LAYER = 5
+ACTIVATION = 'relu'
+INPUT_LAYER_SIZE = 32
+HIDDEN_LAYER_SIZE = 128
+KERNEL_SIZE = (2, 2)
+CONV_STRIDES = (1, 1)
+POOL_STRIDES = (2, 2)
+POOL_SIZE = (2, 2)
+DROPOUT = 0.2
+LOSS = 'categorical_crossentropy'
+OPTIMIZER = 'adam'
+METRICS = ['acc']
+
+BATCH_SIZE = 10
+EPOCHS = 10
--- src/load_data.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-
-def load_data(file_name):
-    with open(file_name, 'rb') as f:
-        data = pickle.load(f)
-    return data
